977ba63141bcc129175025c05ad97f92677e67b1
jzhang999 <56798336+jzhang999@users.noreply.github.com>
2021-02-23 22:55:52 +0800

    Update query3.sql

--- p1/query3.sql
+++ p1/query3.sql
@@ -1,9 +1,8 @@
 WITH NumCategories(ID, Num) as
     (SELECT Categories.ItemID, COUNT(Categories.Category) AS Num_categories
-    FROM Categories, Bids
-    WHERE Categories.ItemID = Bids.ItemID
+    FROM Categories
     GROUP BY Categories.ItemID)
 
 SELECT COUNT(*)
-FROM NumCategories
-WHERE Num = 4;
+FROM NumCategories, Bids
+WHERE NumCategories.Num = 4 AND NumCategories.ID = Bids.ItemID;
