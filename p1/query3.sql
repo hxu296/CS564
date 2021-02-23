f04d5d8dc4d3f6f73055fa9174c6324269a43ec6
jzhang999 <56798336+jzhang999@users.noreply.github.com>
2021-02-23 23:21:48 +0800

    Update query3.sql

--- p1/query3.sql
+++ p1/query3.sql
@@ -1,8 +1,14 @@
 WITH NumCategories(ID, Num) as
-    (SELECT Categories.ItemID, COUNT(Categories.Category) AS Num_categories
+    (SELECT Categories.ItemID, COUNT(Categories.Category)
     FROM Categories
-    GROUP BY Categories.ItemID)
+    GROUP BY Categories.ItemID),
+     CombinedBids(ID, Num) as
+    (SELECT Bids.ItemID, COUNT(Bids.ItemID)
+    FROM Bids
+    GROUP BY Bids.ItemID)
 
-SELECT COUNT(*)
-FROM NumCategories, Bids
-WHERE NumCategories.Num = 4 AND NumCategories.ID = Bids.ItemID;
+SELECT SUM(NumCategories.Num*CombinedBids.Num)
+FROM NumCategories, CombinedBids
+WHERE NumCategories.Num = 4 AND NumCategories.ID = CombinedBids.ID;
+
+
