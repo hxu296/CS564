5b0a6a01d518690ce8b0dd45479394bfa8c5647e
jzhang999 <56798336+jzhang999@users.noreply.github.com>
2021-04-02 21:34:51 +0800

    Update buffer.cpp

--- p2/BufMgr/src/buffer.cpp
+++ p2/BufMgr/src/buffer.cpp
@@ -57,17 +57,12 @@ BufMgr::~BufMgr()
 
 void BufMgr::advanceClock()
 {
-	// Advances clock to the next frame in the buffer pool.
+	// Advances clock to the next frame in the buffer pool
 	clockHand = (clockHand + 1) % numBufs;
 }
 
 void BufMgr::allocBuf(FrameId & frame) 
 {
-    // Allocates a free frame using the clock algorithm;
-    // Throws BufferExceededException if all buffer frames are pinned.
-    // This private method will get called by the readPage() and allocPage() methods.
-    // Makesure that if the buffer frame allocated has a valid page in it,
-    // you remove the appropriate entry from the hash table.
     advanceClock();
     FrameId startID = clockHand; // start frame
     uint32_t numPinned = 0; // number of frames that are pinned
@@ -116,13 +111,10 @@ void BufMgr::allocBuf(FrameId & frame)
 	
 void BufMgr::readPage(File* file, const PageId pageNo, Page*& page)
 {
-	// First check whether the page is already in the buffer pool by invoking 
-	// the lookup() method, which may throw HashNotFoundException when page is not in the buffer pool, 
-	// on the hashtable to get a frame number.
 	bool found = true; // if the page is found
 	FrameId	frameNo;
 	try {
-		hashTable->lookup(file, pageNo, frameNo); // address of??
+		hashTable->lookup(file, pageNo, frameNo); // todo: address of??
 	}
 	catch (const HashNotFoundException &e) {
 		found = false; // throw exception when page is not in the buffer pool
@@ -131,30 +123,23 @@ void BufMgr::readPage(File* file, const PageId pageNo, Page*& page)
 	// if not found
 	if (!found) {
 		// Call allocBuf() to allocate a buffer frame 
-		// and then call the method file->readPage() to read the page from disk into the buffer pool frame. 
-		// Next, insert the page into the hashtable. Finally, invoke Set() on the frame to set it up properly.
-		// Return a pointer to the frame containing the page via the page parameter.
 		allocBuf(frameNo);
+		// read the page from disk into the buffer pool frame
 		bufPool[frameNo] = file->readPage(pageNo);
         hashTable->insert(file, pageNo, frameNo);
         bufDescTable[frameNo].Set(file, pageNo);
         page = bufPool + frameNo;
 	}
 	else {
-		// In this case set the appropriate refbit, increment the pinCnt for the page, 
-		// and then return a pointer to the frame containing the page via the page parameter.
 		bufDescTable[frameNo].refbit = true;
 		bufDescTable[frameNo].pinCnt++;
+		// return a pointer to the frame containing the page
 		page = bufPool + frameNo;
 	}
 }
 
 void BufMgr::unPinPage(File* file, const PageId pageNo, const bool dirty) 
 {
-	// Decrements the pinCnt of the frame containing (file, PageNo) and, if dirty == true, 
-	// sets the dirty bit. Throws PAGENOTPINNED if the pin count is already 0. 
-	// Does nothing if page is not found in the hash table lookup.
-
 	// use hash table lookup to find the frame number of specified page in given file
 	bool found = true; // if the page is found
 	FrameId	frameNo;
@@ -168,7 +153,7 @@ void BufMgr::unPinPage(File* file, const PageId pageNo, const bool dirty)
 	// do nothing if not found
 	if (!found) return;
 
-	// Throws PAGENOTPINNED if the pin count is already 0
+	// Throws PAGENOTPINNEDException if the pin count is already 0
 	if (bufDescTable[frameNo].pinCnt == 0) {
 		throw PageNotPinnedException(file->filename(), pageNo, frameNo);
 	}
@@ -183,13 +168,6 @@ void BufMgr::unPinPage(File* file, const PageId pageNo, const bool dirty)
 
 void BufMgr::allocPage(File* file, PageId &pageNo, Page*& page) 
 {
-	// The first step in this method is to allocate an empty page in the 
-	// specified file by invoking the file->allocatePage() method.
-	// This method will return a newly allocated page.
-	// The method returns both the page number of the newly allocated page 
-	// to the caller via the pageNo parameter and a pointer to the buffer frame allocated 
-	// for the page via the page parameter.
-
 	// AllocBuf() is called to obtain a buffer pool frame.
 	FrameId	frameNo;
 	allocBuf(frameNo);
@@ -199,7 +177,7 @@ void BufMgr::allocPage(File* file, PageId &pageNo, Page*& page)
     page = &bufPool[frameNo];
     pageNo = page->page_number();
 
-	// Next, an entry is inserted into the hash table and Set() is invoked on the frame to set it up properly. 
+	// entry inserted into the hash table and Set() the frame
 	hashTable->insert(file, pageNo, frameNo);
 	bufDescTable[frameNo].Set(file, pageNo);
 }
@@ -209,7 +187,7 @@ void BufMgr::flushFile(const File* file)
 	// Should scan bufTable for pages belonging to the file.
 	for (FrameId i = 0; i < numBufs; i++) {
 		if (bufDescTable[i].file == file) { // page belongs to the file
-            // Throws BadBuffer- Exception if an invalid page belonging to the file is encountered
+            // Throws BadBufferException if an invalid page
             if (!bufDescTable[i].valid) {
                 throw BadBufferException(bufDescTable[i].frameNo, bufDescTable[i].dirty, bufDescTable[i].valid, bufDescTable[i].refbit);
             }
@@ -217,8 +195,7 @@ void BufMgr::flushFile(const File* file)
             if (bufDescTable[i].pinCnt != 0) {
                 throw PagePinnedException(file->filename(), bufDescTable[i].pageNo, bufDescTable[i].frameNo);
             }
-			// if the page is dirty, call file->writePage()
-			// to flush the page to disk and then set the dirty bit for the page to false
+			// if the page is dirty, flush the page to disk, set the dirty bit to false
 			if (bufDescTable[i].dirty) {
 				bufDescTable[i].file->writePage(bufPool[bufDescTable[i].frameNo]);
 				bufDescTable[i].dirty = false;
@@ -231,9 +208,6 @@ void BufMgr::flushFile(const File* file)
 
 void BufMgr::disposePage(File* file, const PageId PageNo)
 {
-	// This method deletes a particular page from file. Before deleting the page from file, 
-	// it makes sure that if the page to be deleted is allocated a frame in the buffer pool, 
-
 	// use hash table lookup to find the frame number of specified page in given file
 	bool found = true;
 	FrameId	frameNo;
